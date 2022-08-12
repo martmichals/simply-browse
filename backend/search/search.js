a71f027412cfc293fa41fe0cee8d1ecef30cea13
Martin Michalski <martmichals@gmail.com>
2022-08-12 15:54:04 -0500

    Implemented bing querying logic and throttling

---
+++ backend/search/search.js
@@ -0,0 +1,93 @@
+// Dependencies
+const https = require('https')
+const Bottleneck = require('bottleneck/es5')
+
+// API Details and limiter
+const BING_KEY = require('../secrets.json').BING_KEY
+const BING_LIMITER = new Bottleneck({
+    maxConcurrent: 3,
+    minTime: 333
+})
+
+parseQuery = function(req) {
+    // Check if query is defined
+    if (req.query.type === undefined) {
+        console.error('Bad request to parseQuery')
+        throw 'Request query is missing required parameters!'
+    }
+
+    // Parse query based on type
+    switch (req.query.type) {
+        case '2term':
+            // Handle 2 term query request
+            if (typeof req.query.term1 == 'string' || typeof req.query.term2 == 'string') {
+                return '"' + req.query.term1 + '" "' + req.query.term2 + '"'
+            } else {
+                console.error('Bad 2term argument specification')
+                throw 'Query\'s term1 and/or term2 are not properly specified!'
+            }
+        default:
+            console.error('Bad query type specification')
+            throw 'The query type specified, ' + req.query.type + ' has no associated handler!'
+    } 
+}
+
+module.exports.searchBing = async function (req, res) {
+    // Parse the query
+    const queryString = parseQuery(req)
+
+    // TODO: Check the server cache for the response
+
+    // Create the query
+    const query = new URLSearchParams({
+        mkt:            'en-US',
+        count:           50,
+        q:               queryString,
+        responseFilter: 'webpages',
+        setLang:        'en-US',
+        textFormat:     'HTML'
+    }).toString()
+    console.log('Querying the Bing Search API with parameters: ' + query)
+
+    // Launch request to Bing API
+    BING_LIMITER.submit(
+        https.get, 
+        {
+            hostname: 'api.bing.microsoft.com',
+            path:     '/v7.0/search?q=' + query,
+            headers:  { 'Ocp-Apim-Subscription-Key': BING_KEY }
+        },
+        resBing => {
+            let body = ''
+            resBing.on('data', part => body += part)
+            resBing.on('end', () => {
+                // Filter out unnecessary components of the response
+                const searchResults = JSON.parse(body)
+
+                // Construct the server response from the search results
+                let serverResponse = { 
+                    pages: [],
+                    pageCount: 0
+                }
+                for (const page of searchResults.webPages.value) {
+                    serverResponse.pages.push({
+                        name:       page.name,
+                        url:        page.url,
+                        displayUrl: page.displayUrl,
+                        snippet:    page.snippet
+                    })
+                    serverResponse.pageCount++
+                }
+
+                // TODO: Populate the server cache with the response
+
+                // Send the server response
+                res.status(201).send(serverResponse)
+            })
+            resBing.on('error', err => {
+                console.error('Error: ' + err.message)
+                throw err
+            })
+        }
+    )
+}
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,7 @@
-// Dependencies
+// Local dependencies
+const search = require('./search/search')
+
+// General dependencies
 const cors = require('cors')
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -13,9 +16,16 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // Set up endpoints
-app.get('/search', (req, res) => {
-    console.log('Hit the search endpoint')
-    // TODO: Set up queue for incoming search requrests to enforce TPS limit on Bing API
+app.get('/search', async (req, res) => {
+    // Call function to get search results
+    try {
+        await search.searchBing(req, res)
+    } catch(err) {
+        console.error(err)
+        res.status(500).send({ message: 'Internal Server Error! See server logs for details.' })
+    }
+
+    // TODO: Set up queue for incoming search requests to enforce TPS limit on Bing API
     // TODO: Set up persistent DB solution to store requests in the month, TPS load for the second
     // TODO: Set up Bing API querying (conditional on abscence from local cache)
     // TODO: Set up local persistent DB solution to store request results
@@ -31,4 +41,4 @@ app.get('/view', (req, res) => {
 })
 
 // Start app
-app.listen(port, () => { console.log('API live on port ' + port) })
+app.listen(port, () => { console.log('API live on port local' + port) })
